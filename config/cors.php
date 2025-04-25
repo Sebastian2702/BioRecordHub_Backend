bfe6fb565e5fa98ed62ffbc173602a819a704231
Sebastian <sebastiangoncalves2002@gmail.com>
2025-04-25 21:07:33 +0100

    trying to make login work

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,14 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => [
+        'api/*',
+        'login',
+        'logout',
+        'sanctum/csrf-cookie',
+        'register',
+        '/user'
+    ],
 
     'allowed_methods' => ['*'],
 
