ac2e4e6815c6dce63f532de04a7f1bfc69946cb5
Sebastian <sebastiangoncalves2002@gmail.com>
2025-06-28 17:40:15 +0100

    changed request rule for date field

--- app/Http/Requests/StoreBibliographyRequest.php
+++ app/Http/Requests/StoreBibliographyRequest.php
@@ -33,7 +33,7 @@ class StoreBibliographyRequest extends FormRequest
             'doi' => 'nullable|string|max:255',
             'url' => 'nullable|url',
             'abstract_note' => 'nullable|string',
-            'date' => 'required|date',
+            'date' => 'required|digits:4',
             'date_added' => 'nullable|date',
             'date_modified' => 'nullable|date',
             'access_date' => 'nullable|date',
